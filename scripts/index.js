8d975a3881f6609122f0c7377e205c514ee51a6d
Flaki <git@flaki.hu>
2018-07-01 00:08:06 +0200

    editor with ES modules

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+import * as editor from './lib.js';
+console.log(editor)
+
+var form = new editor.Form({
+    el: '.form',
+    effect: '0,,0.2561,,0.4667,0.3152,,0.1037,,0.2768,0.1347,,,0.236,,,,,1,,,,,0.5'
+});
---
+++ scripts/lib.js
@@ -0,0 +1,7 @@
+import Effect from './effect.js'
+import Form from './form.js'
+
+console.log(Effect)
+console.log(Form)
+
+export { Effect, Form }
